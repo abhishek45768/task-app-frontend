34bbdd9dff5fa65d58e6b18102582eb4d8f29e69
abhishek45768 <abhishekbhardwaj0046@gmail.com>
2024-09-17 22:59:28 +0530

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header({ onAddtask }) {
   return (
     <header className="header">
       <h1>My Task</h1>
-      <button className="add-note-btn" onClick={onAddtask}>
+      <button className="add-task-btn" onClick={onAddtask}>
         + Add New TASK
       </button>
     </header>
